d4898f0f49b0b8e36aa7b64f8d898104e7fded81
Christian Tellnes <christian@tellnes.no>
2013-04-03 00:51:45 +0200

    add logging

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 
 var express = require('express')
   , path = require('path')
+  , config = require('./config')
 
 var app = module.exports = express()
 
 app.enable('trust proxy')
 
+app.use(express.logger(config.get('env') === 'development' ? 'dev' : 'default'))
 app.use(express.favicon())
 app.use('/f', require('./form'))
 app.use(express.static(path.resolve(__dirname, 'client')))
